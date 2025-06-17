3a301ab00536863eab2802efbcdf2b4bd39002c5
rafaelSacata <rsaranda93@outlook.com>
2025-06-17 02:13:42 -0300

    Removing unused imports in UserEntity class

--- src/main/java/br/com/rafaelaranda/task_manager/user/entity/UserEntity.java
+++ src/main/java/br/com/rafaelaranda/task_manager/user/entity/UserEntity.java
@@ -7,7 +7,6 @@ import br.com.rafaelaranda.task_manager.task.entity.TaskEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
