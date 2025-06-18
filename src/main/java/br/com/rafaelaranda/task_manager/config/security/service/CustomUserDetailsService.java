9a9c9801623941222ed6b4e0adf9b60434b932df
rafaelSacata <rsaranda93@outlook.com>
2025-06-18 17:43:02 -0300

    correções gerais no código

---
+++ src/main/java/br/com/rafaelaranda/task_manager/config/security/service/CustomUserDetailsService.java
@@ -0,0 +1,6 @@
+package br.com.rafaelaranda.task_manager.config.security.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class CustomUserDetailService implements UserDetailsService {
+}
